63b8d4ecfd17cf304054b375650cdf20003d29db
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-11-26 20:56:21 +0900

    feat. slide 추가

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,6 +2,14 @@ $('.post-wrapper').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: false,
-  nextArrow:$('.next'),
-  prevArrow:$('.prev'),
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+});
+
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
