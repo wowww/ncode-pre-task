ecddf8932769e22c3c01684ff6ea15238560acc3
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-11-27 01:22:22 +0900

    feat. 변수 선언 방식 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,22 @@
-(function(){
-  var d = document,
-      tabs = d.querySelector('.tabs'),
-      tab = d.querySelectorAll('li'),
-      contents = d.querySelectorAll('.content');
-  tabs.addEventListener('click', function(e) {
-      if (e.target && e.target.nodeName === 'LI') {
-        // change tabs
-        for (var i = 0; i < tab.length; i++) {
-          tab[i].classList.remove('active');
-        }
-        e.target.classList.toggle('active');
+(function () {
+  const d = document,
+    tabs = d.querySelector('.tabs'),
+    tab = d.querySelectorAll('li'),
+    contents = d.querySelectorAll('.content');
 
+  tabs.addEventListener('click', function (e) {
+    if (e.target && e.target.nodeName === 'LI') {
+      for (let i = 0; i < tab.length; i++) {
+        tab[i].classList.remove('active');
+      }
+      e.target.classList.toggle('active');
 
-        // change content
-        for (i = 0; i < contents.length; i++) {
-          contents[i].classList.remove('active');
-        }
-        
-        var tabId = '#' + e.target.dataset.tabId;
- d.querySelector(tabId).classList.toggle('active'); 
-      }  
+      for (let i = 0; i < contents.length; i++) {
+        contents[i].classList.remove('active');
+      }
+
+      const tabId = '#' + e.target.dataset.tabId;
+      d.querySelector(tabId).classList.toggle('active');
+    }
   });
 })();
